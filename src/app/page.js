b71f6f1a0abee26c7c0613d77768e7318e406818
edisson89 <edisson.12@hotmail.com>
2025-03-18 16:53:14 -0500

    Codigo lectorXpress

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,116 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+/** @format */
+"use client"
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import React, { useEffect, useState } from "react"
+import { data } from "@/utils/data"
+import Image from "next/image"
+import Footer from "./footer"
+import logo from "../../images/lectorxprees.jpg"
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+const words = data.text.split(" ")
+
+function Lectura() {
+	const initialSpeed = 3
+	const initialIsPlaying = false
+	const [wordCount, setWordCount] = useState(initialSpeed)
+	const [speed, setSpeed] = useState(500) // En milisegundos
+	const [index, setIndex] = useState(0)
+	const [isPlaying, setIsPlaying] = useState(initialIsPlaying)
+	// Calcular palabras por minuto
+	const wordsPerMinute = Math.round((60000 / speed) * wordCount)
+	// Función para resetear el estado
+	const resetState = () => {
+		setWordCount(initialSpeed)
+		setIsPlaying(initialIsPlaying)
+    setIndex(0)
+	}
+
+	useEffect(() => {
+    let interval; // Declarar el intervalo fuera del bloque
+  if (isPlaying) {
+    interval = setInterval(() => {
+      setIndex((prevIndex) => {
+        if (prevIndex >= words.length - wordCount) {
+          return 0;
+        }
+        return prevIndex + wordCount;
+      });
+    }, speed);
+  }
+  // Limpiar el intervalo en el return del useEffect
+  return () => {
+    if (interval) {
+      clearInterval(interval);
+    }
+  };
+	}, [isPlaying, speed, wordCount])
+
+	return (
+		<div className="flex min-h-screen flex-col">
+			<div className="flex items-center p-1 background bg-custom-gradient bg-gradient-to-r from-[#6a64f6] via -[#a96ddf] via -[#c77fcd] to-[#a789c9]">
+				<div className="items-start">
+					<Image
+          className="rounded-full"
+						src={logo}
+						alt="lectorXpress"
+						height={100}
+						width={100}
+					/>
+				</div>
+				<div className='flex w-full h-14 text-amber-200 text-2xl size-3.5 justify-center p-2 m-2'>Lectura Rapida</div>
+			</div>
+
+			<h1 className="p-3 m-5">Resumen del Libro &quot;Los Secretos de la Mente Millonaria&ldquo; de T. Harv Eker</h1>
+			<div className="p-4 text-center space-y-4">
+				<h2 className="m-8 text-xl font-bold">A Que Velocidad Puedes Leer!!!</h2>
+				<p className="m-9 text-2xl font-bold bg-yellow-500 text-black px-4 py-2 rounded-lg">
+					🚀 Puedes leer a: <strong>{wordsPerMinute} WPM(Palabras Por minuto)</strong>
+				</p>
+
+				<div className="flex m-8 justify-center space-x-4">
+					<label>Palabras: </label>
+					<input
+						type="range"
+						min="2"
+						max="10"
+						value={wordCount}
+						onChange={(e) => setWordCount(Number(e.target.value))}
+					/>
+					<span className="flex justify-center">{wordCount}</span>
+				</div>
+
+				<div className="flex m-8 justify-center space-x-4">
+					<label>Velocidad (ms): </label>
+                   
+					<input
+          
+						type="range"
+						min="100" 
+						max="2000"
+						step="100"
+						value={speed}
+						onChange={(e) => setSpeed(Number(e.target.value))}
+					/>
+					<span>{speed}ms</span>
+				</div>
+
+				<button
+					className="m-8 w-25 px-4 py-2 bg-blue-500 text-white rounded-xl"
+					onClick={() => setIsPlaying(!isPlaying)}
+				>
+					{isPlaying ? "Pausar" : "Iniciar"}
+				</button>
+				<button
+					className=" m-8 w-25 px-4 py-2 bg-red-500 text-white rounded-xl"
+					onClick={resetState}
+				>
+					Resetear
+				</button>
+			</div>
+			<div className="flex flex-nowrap h-14 items-center m-8 justify-center p-4 text-2xl font-semibold bg-gray-200 rounded-xl">{words.slice(index, index + wordCount).join(" ")}</div>
+		 <Footer />
     </div>
-  );
+	)
 }
+
+export default Lectura
