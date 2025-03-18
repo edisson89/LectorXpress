44946bf8be3982a3b60bb83b19772d7f250d19ec
edisson89 <edisson.12@hotmail.com>
2025-03-18 16:52:53 -0500

    Creand footer

---
+++ src/app/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='flex w-full justify-center items-center h-13 bg-black'>
+       <h1 className='justify-center  text-amber-50'>Todos los derechos reservados</h1> 
+    </div>
+  )
+}
+
+export default Footer
