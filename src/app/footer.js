3f845398654906d837925ca96a2faa36d8aee95c
edisson89 <edisson.12@hotmail.com>
2025-04-30 15:06:12 -0500

    cambios footer

--- src/app/footer.js
+++ src/app/footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='flex w-full justify-center items-center h-13 bg-black'>
+    <div className='flex w-full mt-10 justify-center items-center h-13 bg-black'>
        <h1 className='justify-center  text-amber-50'>Todos los derechos reservados</h1> 
     </div>
   )
